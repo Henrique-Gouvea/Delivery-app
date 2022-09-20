17d513574147a9c1ea9990404d3825100704ac02
DannyElis89 <dannyelis89@gmail.com>
2022-09-20 19:12:37 -0300

    Requisito 01: início. Cria migrations, seeders e models

---
+++ back-end/src/database/models/users.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+        },
+        email: {
+            type: DataTypes.STRING,
+        },
+        password: {
+            type: DataTypes.STRING,
+        },
+        role: {
+            type: DataTypes.STRING,
+        },
+    },
+        {
+            timestamps: false,
+            tableName: 'Users',
+        });
+
+        User.associate = (models) => {
+            User.hasMany(models.Sale, { foreignKey: 'user_id', as: 'sale' });
+        }
+
+    return User;
+};
