14dbab24925d57f19d5b1917d05b7a0e3808120d
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-09-21 08:22:34 -0300

    feat(Services): Criando request api e intalando axios

---
+++ front-end/src/services/api.jsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const request = async (resource, method, body) => {
+  const { name, password } = body;
+  try {
+    const res = await axios[method](`http://localhost:3001${resource}`, {
+      name,
+      password,
+    });
+    return res.data;
+  } catch (err) { return (err.response.data); }
+};
+
+export default apiRequestRegister = async (user) => (
+  request('/login', 'post', user));
