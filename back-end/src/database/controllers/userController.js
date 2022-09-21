fd4bf2879d7d0119f3e0f4e2a755247f84d8d265
DannyElis89 <dannyelis89@gmail.com>
2022-09-21 19:08:06 -0300

    push para mergiar e testar o back-end

--- back-end/src/database/controllers/userController.js
+++ back-end/src/database/controllers/userController.js
@@ -1,12 +1,21 @@
 const userService = require('../services/userService');
+import { StatusCodes } from "http-status-codes";
+
+const createUser = async (req, res, next) => {
+  const user = req.body;
+
+  const newUser = await userService.createUser(user);
+  res.status(StatusCodes.CREATED).json(newUser);
+}
 
 const findAll = async (_req, res) => {
   const allUsers = await userService.findAll();
   console.log(allUsers)
 
-  res.status(200).json(allUsers);
+  res.status(StatusCodes.OK).json(allUsers);
 };
 
 module.exports = {
   findAll,
+  createUser,
 }
--- back-end/src/database/services/userService.js
+++ back-end/src/database/services/userService.js
@@ -1,11 +1,33 @@
 const { User } = require("../models");
+import { StatusCodes } from 'http-status-codes';
+const { encriptPassword } = require("../middlewares/createToken");
+const { createToken } = require("../middlewares/createToken");
+
+const userSchema = Joi.object({
+  name: Joi.string().require,
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+const createUser = async (user) => {
+  const { error } = userSchema.validate(user);
+  if (error) return sendError(StatusCodes.NOT_FOUND, "Some required fields are missing");
+
+  const passHash = encriptPassword(user.password);
+  const newUser = await User.create({ ...user, password: passHash, role: 'customer' });
+
+  const token = createToken({ email: user.email, password: user.password });
+
+  return { token, role: newUser.role }
+
+}
 
 const findAll = async () => {
   const allUsers = await User.findAll({
     attributes: ["id", "name", "email", "role"],
   });
   console.log('userService');
-  console.log('allUsers', allUsers)
+  console.log('allUsers', allUsers);
 
   return allUsers;
 };
@@ -13,4 +35,5 @@ const findAll = async () => {
 
 module.exports = {
   findAll,
+  createUser,
 };
