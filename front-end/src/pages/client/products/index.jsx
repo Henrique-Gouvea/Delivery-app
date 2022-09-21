5a089e31851d92395c0106e27329e10edd81c260
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-09-21 16:46:27 -0300

    feat(Components): Criando card para produtos

---
+++ front-end/src/pages/client/products/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../../components/header';
+import ProductCard from '../../../components/productCard';
+
+function Products() {
+  return (
+    <div>
+      <Header />
+      <ProductCard />
+    </div>
+
+  );
+}
+export default Products;
--- front-end/src/routes/routesApp.jsx
+++ front-end/src/routes/routesApp.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from '../pages/login';
 import Cadaster from '../pages/cadaster';
+import Products from '../pages/client/products';
 
 function RoutesApp() {
   return (
     <Routes>
-      <Route exact path="/" element={ <Login /> } />
+      <Route exact path="/" element={ <Navigate to="/login" /> } />
       <Route exact path="/login" element={ <Login /> } />
       <Route exact path="/register" element={ <Cadaster /> } />
+      <Route exact path="/customer/products" element={ <Products /> } />
 
     </Routes>
   );
