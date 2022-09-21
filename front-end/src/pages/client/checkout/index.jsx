6e712f3738bcf0b5887f2a7481aabc2088f8c114
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-09-21 17:57:04 -0300

    feat(Pages): Criando pagina de checkout

--- front-end/src/pages/client/checkout/index.jsx
+++ front-end/src/pages/client/checkout/index.jsx
@@ -7,10 +7,41 @@ function Checkout() {
 
     <div>
       <Header />
+      <h2>Finalizar Pedido</h2>
       <div>
         <TableCheckout />
         <p data-testid="customer_checkout__element-order-total-price">Valor Total:</p>
       </div>
+      <div>
+        <h2>
+          Detalhes e Endereço de entrega
+        </h2>
+        <p>
+          Vendedor responsabel
+        </p>
+        <select
+          data-testid="customer_checkout__select-seller"
+          name="select"
+        >
+          <option value="valor1">Valor 1</option>
+          <option value="valor2" selected>Valor 2</option>
+          <option value="valor3">Valor 3</option>
+        </select>
+        <p>Endereço</p>
+        <input
+          data-testid="customer_checkout__input-address"
+        />
+        <p>Numero</p>
+        <input
+          data-testid="customer_checkout__input-address-number"
+        />
+      </div>
+      <button
+        type="submit"
+        data-testid="customer_checkout__button-submit-order"
+      >
+        Finalizar pedido
+      </button>
     </div>
 
   );
