fdb3f359f841be90d4379a0774289f918ea1367f
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-09-28 09:46:49 -0300

    feat(Components): Adicionando proptypes ao ProdCard

--- front-end/src/components/prodCard/index.jsx
+++ front-end/src/components/prodCard/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   addProductStorage,
   getCartTotal,
@@ -127,4 +128,9 @@ class ProdCard extends Component {
     );
   }
 }
+
+ProdCard.propTypes = {
+  changeTotal: PropTypes.function,
+}.isRequired;
+
 export default ProdCard;
