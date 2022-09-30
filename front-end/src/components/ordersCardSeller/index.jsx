2d42f97b0fa0cb0a009ad3ed921121544eba429d
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-09-30 19:41:10 -0300

    feat(Pages): Requisito 29 ok

--- front-end/src/components/ordersCardSeller/index.jsx
+++ front-end/src/components/ordersCardSeller/index.jsx
@@ -1,38 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getStorageOrder } from '../../helpers/localStorageOrderDdetails';
+import { getStorageUser } from '../../helpers/localStorage';
 
-function OrdersCard() {
-  const id = '3';
-  return (
-    <div>
+class OrdersCardSeller extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orders: '',
+    };
+  }
+
+  componentDidMount() {
+    const orders = getStorageOrder();
+    const user = getStorageUser();
+    const orderFiltered = orders ? orders.filter((ord) => ord.seller_id === user.id) : [];
+    console.log(orderFiltered);
+    this.setState({
+      orders: orderFiltered,
+    });
+  }
+
+  convertDate = (date) => {
+    const VALUE_REMOVED_DATE = 10;
+    const removedDate = date.substr(0, VALUE_REMOVED_DATE);
+    const dateFormatted = removedDate.split('-').reverse().join('/');
+    return dateFormatted;
+  };
+
+  render() {
+    const {
+      orders,
+    } = this.state;
+    return (
       <div>
-        <p
-          data-testid={ `seller_orders__element-order-id-${id}` }
-        >
-          Pedido ID
-        </p>
-        <p
-          data-testid={ `seller_orders__element-delivery-status-${id}` }
-        >
-          Status Pedido
-        </p>
-        <p
-          data-testid={ `seller_orders__element-order-date-${id}` }
-        >
-          Data Pedido
-        </p>
-        <p
-          data-testid={ `seller_orders__element-card-price-${id}` }
-        >
-          Valor total
-        </p>
-        <p
-          data-testid={ `seller_orders__element-card-address-${id}` }
-        >
-          Endereço
-        </p>
+        {orders ? orders.map((ord) => (
+          <Link to={ `/seller/orders/${ord.id}` } key={ ord.id }>
+            <div>
+              <p
+                data-testid={ `seller_orders__element-order-id-${ord.id}` }
+              >
+                {ord.id}
+              </p>
+              <p
+                data-testid={ `seller_orders__element-delivery-status-${ord.id}` }
+              >
+                {ord.status}
+              </p>
+              <p
+                data-testid={ `seller_orders__element-order-date-${ord.id}` }
+              >
+                {ord.sale_date ? this.convertDate(ord.sale_date) : ''}
+              </p>
+              <p
+                data-testid={ `seller_orders__element-card-price-${ord.id}` }
+              >
+                {ord.total_price.toString().replace('.', ',')}
+              </p>
+              <p
+                data-testid={ `seller_orders__element-card-address-${ord.id}` }
+              >
+                {`${ord.delivery_address} ${ord.delivery_number}`}
+              </p>
 
+            </div>
+          </Link>
+        )) : ''}
       </div>
-    </div>
-  );
+    );
+  }
 }
-export default OrdersCard;
+export default OrdersCardSeller;
