28c256ef6496c30bb541e170c5688bfa2bd5cd90
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-10-03 15:00:18 -0300

    Mergeando com PR 708

--- front-end/src/pages/client/orders/index.jsx
+++ front-end/src/pages/client/orders/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
 import Header from '../../../components/header';
 import OrdersCard from '../../../components/ordersCard';
 
